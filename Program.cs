a8a6209e241065d0c3d214fa0710e13156181479
24ankur <69760150+24ankur@users.noreply.github.com>
2021-02-21 18:40:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BinaryTree
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    int i = 0;
+    Node root;
+    public Node rec(string[] str, int n)
+    {
+        Node root = new Node(int.Parse(str[i]));
+        i++;
+        if (str[i] == "true")
+        {
+            i++;
+            root.left = rec(str, n);
+        }
+        else
+            i++;
+        if (str[i] == "true")
+        {
+            i++;
+            root.right = rec(str, n);
+        }
+        else
+            i++;
+        return root;
+    }
+    public void CreateBT()
+    {
+        string[] str = Console.ReadLine().Split();
+        int n = str.Length;
+        i = 0;
+        root = rec(str, n);
+    }
+
+    public int Sum()
+    {
+        return Sum(root);
+    }
+
+    public int Sum(Node root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        return root.data + Sum(root.left) + Sum(root.right);
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.CreateBT();
+            int Value = bt.Sum();
+            Console.Write(Value);
+            Console.Read();
+        }
+    }
+}
